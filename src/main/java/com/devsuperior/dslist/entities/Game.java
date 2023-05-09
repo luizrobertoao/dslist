e0591cb8b109670c880041dfc8b9a3e61866b833
Luiz Roberto <luizrobertoao29@gmail.com>
2023-05-09 20:43:45 -0300

    Game repository, service, controller.

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dslist.entities;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;

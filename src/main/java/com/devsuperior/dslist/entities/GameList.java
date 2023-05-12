97ead2dc8c354c3908a680d183c288bd908b4523
Luiz Roberto <luizrobertoao29@gmail.com>
2023-05-12 18:33:29 -0300

    GET lists.

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -15,6 +15,9 @@ public class GameList {
     private Long id;
     private String name;
     
+    public GameList() {
+    }
+
     public GameList(Long id, String name) {
         this.id = id;
         this.name = name;
